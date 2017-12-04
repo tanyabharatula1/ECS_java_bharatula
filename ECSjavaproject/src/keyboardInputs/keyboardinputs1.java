458dcf1b98d594da830083c33496c66251097849
tanyabharatula1 <BharatulaT9696@sduhsd.lan>
2017-12-04 10:14:28 -0800

    gramps

---
+++ ECSjavaproject/src/keyboardInputs/keyboardinputs1.java
@@ -0,0 +1,35 @@
+package keyboardInputs;
+
+import java.util.Scanner;
+
+public class keyboardinputs1 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		String name;
+		int age, feet, inches;
+		double weight;
+		
+		System.out.println("what is your name? " );
+		name = keyboard.next();
+		
+		System.out.println("How old are you? " );
+		age = keyboard.nextInt();
+		
+		System.out.println("How many feet tall are you? " );
+		feet = keyboard.nextInt();
+		
+		System.out.println("How many inches? " );
+		inches =- keyboard.nextInt();
+		
+		System.out.println("How much do you weigh? " );
+		weight = keyboard.nextDouble();
+		
+		System.out.println("So you're name is " + name + ", you are " + age + "years old, you're" + feet + "'" + inches + ",and you weigh " + weight + "pounds.");
+		
+		keyboard.close();
+	}
+
+}
