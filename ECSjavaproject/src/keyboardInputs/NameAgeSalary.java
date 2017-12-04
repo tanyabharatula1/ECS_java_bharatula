4339924b4a078cbd29bd8a006c5f7a222c4751aa
tanyabharatula1 <BharatulaT9696@sduhsd.lan>
2017-12-04 10:21:41 -0800

    one more done on assign 3

---
+++ ECSjavaproject/src/keyboardInputs/NameAgeSalary.java
@@ -0,0 +1,29 @@
+package keyboardInputs;
+
+import java.util.Scanner;
+
+public class NameAgeSalary 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		String name;
+		int age;
+		double salary;
+		
+		System.out.println("Hello. What is your name? " );
+		name = keyboard.next();
+		
+		System.out.println("Hi, " + name + " How old are you? " );
+		age = keyboard.nextInt();
+		
+		System.out.println("So you are " + age + "?  I guess that's not too old " + name + ".  how much do you make per hour?" );
+		salary = keyboard.nextDouble();
+		
+		System.out.println("Wow,  " + salary + " is a lot for your age! ");
+		
+		keyboard.close();
+	}
+
+}
