bfa727b62d1c3c972725110de4bc5f93c06925a1
tanyabharatula1 <BharatulaT9696@sduhsd.lan>
2017-12-07 10:56:34 -0800

    guess

---
+++ ECSjavaproject/src/wHILELOOPSSSSS/WhileLoop1.java
@@ -0,0 +1,25 @@
+package wHILELOOPSSSSS;
+import java.util.Scanner;
+public class WhileLoop1 
+{
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		int pin = 12345;
+		
+		System.out.println("WELCOME TO THE BANK OF MITCHELL");
+		System.out.println("ENTER YOUR PIN: ");
+		int entry = keyboard.nextInt();
+		
+		while ( entry != pin )
+		{
+			System.out.println("\nINCORRECT PIN. TRY AGAIN.");
+			System.out.println("ENTER YOUR PIN:");
+			entry = keyboard.nextInt();
+		}
+		
+		System.out.println("\nPIN ACCEPTED. YOU NOW HAVE ACCESS TO YOUR ACCOUNT.");
+		
+	}
+
+}
---
+++ ECSjavaproject/src/whileLoops/ShorterDoubleDice.java
@@ -0,0 +1,5 @@
+package whileLoops;
+
+public class ShorterDoubleDice {
+
+}
